3afedf281b0c0252988d70c85a83c2f423d6c2cf
aryabhatt00 <aryabhatt.kankipati@gmail.com>
2025-02-12 20:04:49 -0600

    reume_updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import profile_img from '../../assets/arya.svg'
+import resume from '../../assets/resume.pdf'
 
 const Hero = () => {
     return (
@@ -11,7 +12,7 @@ const Hero = () => {
             <p>I am a Software Engineer with experience in web development and data analytics. I have expertise in HTML, CSS, JavaScript, React, AWS, Python, Java, C, Flask, SQL.</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Arya</AnchorLink></div>
-                <div className="hero-resume"><a href="/Arya's Resume.pdf" download="Arya_Resume.pdf">
+                <div className="hero-resume"><a href={resume} download="Arya_Resume.pdf">
               Download Resume
           </a></div>
 
